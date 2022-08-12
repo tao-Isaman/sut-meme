901be4c8d84c336bc6644df93744a05d2f185005
tao-isaman <towlovelove@gmail.com>
2022-08-13 03:40:22 +0700

    remove to do app

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -22,9 +22,6 @@
 			<li class:active={$page.url.pathname === '/about'}>
 				<a sveltekit:prefetch href="{base}/about">About</a>
 			</li>
-			<li class:active={$page.url.pathname === '/todos'}>
-				<a sveltekit:prefetch href="{base}/todos">Todos</a>
-			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
