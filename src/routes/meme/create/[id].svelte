abccb186b1250fd4b0c43eda9beb00ea8b37382c
tao-isaman <towlovelove@gmail.com>
2022-08-14 18:16:16 +0700

    fix style in meme page and add create page

---
+++ src/routes/meme/create/[id].svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+
+</script>
+
+<svelte:head>
+	<title>Create Meme</title>
+	<meta name="Create Memer" content="Create Meme" />
+</svelte:head>
+
+<div class="flex justify-center">
+	<h1>Create Meme</h1>
+</div>
+<div class="flex justify-center">
+	<p>Meme ID is : {$page.url.pathname.split('/')[3]}</p>
+</div>
+
+
+<style>
+</style>
--- src/routes/meme/index.svelte
+++ src/routes/meme/index.svelte
@@ -25,15 +25,15 @@
 </div>
 <div class="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-1 gap-20 p-20">
 	{#each memes as meme (meme.id)}
-		<div class="flex flex-col justify-between hover:bg-gray-200 hover:cursor-pointer">
+		<a
+			class="flex flex-col justify-between hover:bg-gray-200 hover:cursor-pointer grid-meme "
+			href="/meme/create/{meme.id}"
+		>
 			<div class="grid grid-rows-auto content-start pb-5">
 				<p class="text-lg">{meme.name}</p>
 				<img class="object-contain max-h-48" src={meme.url} alt={meme.name} />
 			</div>
-			<button class="bg-orange-300 hover:bg-orange-600 active:bg-orange-700 focus:outline-none focus:ring focus:ring-orange-300 rounded-lg">
-				Add
-			  </button>
-		</div>
+		</a>
 	{/each}
 </div>
 
