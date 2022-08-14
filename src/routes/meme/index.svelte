2bc3ad4ef00e9b17b0127bde5ded1cd2ba828d30
tao-isaman <towlovelove@gmail.com>
2022-08-14 17:07:07 +0700

    add botton

--- src/routes/meme/index.svelte
+++ src/routes/meme/index.svelte
@@ -23,13 +23,16 @@
 <div class="flex justify-center">
 	<h1>Meme Generator</h1>
 </div>
-<div class="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-1 gap-2 p-20">
+<div class="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-1 gap-20 p-20">
 	{#each memes as meme (meme.id)}
-		<div class="flex justify-center hover:bg-gray-200 hover:cursor-pointer">
-			<div class="grid grid-rows-auto content-start">
+		<div class="flex flex-col justify-between hover:bg-gray-200 hover:cursor-pointer">
+			<div class="grid grid-rows-auto content-start pb-5">
 				<p class="text-lg">{meme.name}</p>
-				<img class="object-contain" src={meme.url} alt={meme.name} />
+				<img class="object-contain max-h-48" src={meme.url} alt={meme.name} />
 			</div>
+			<button class="bg-orange-300 hover:bg-orange-600 active:bg-orange-700 focus:outline-none focus:ring focus:ring-orange-300 rounded-lg">
+				Add
+			  </button>
 		</div>
 	{/each}
 </div>
